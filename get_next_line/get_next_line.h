2eda124874014c401aedba076bb62004348e3a4b
Enzo <enzo@DESKTOP-6SU0CE3>
2024-11-15 22:39:02 +0100

    gnl v 0.95

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,20 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: engiacom <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: engiacom <engiacom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/14 12:22:30 by engiacom          #+#    #+#             */
-/*   Updated: 2024/11/14 16:07:08 by engiacom         ###   ########.fr       */
+/*   Created: 2024/11/15 20:25:08 by engiacom          #+#    #+#             */
+/*   Updated: 2024/11/15 22:36:02 by engiacom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_newline(char *s);
+char	*ft_newstash(char *s);
+char	*ft_read(char *s, int fd);
+int		ft_strlen(char *s);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_chr_c(char *s, int c);
+char	*ft_strjoin(char *s, char *b);
 
 #endif
